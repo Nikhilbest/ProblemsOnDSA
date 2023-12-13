62aa6d9e03c1427e2f8b6a5c5e9c35fc6c3c64cf
NIKHIL <103302893+Nikhilbest@users.noreply.github.com>
2023-12-13 17:54:33 +0530

    Create ValidAnagram.py
    to check where words are anagram or not

---
+++ ValidAnagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = defaultdict(int)
+
+        # check for letter in dictionary, increase count
+        for letter in s:
+            d[letter] += 1
+        
+        for letter in t:
+            d[letter] -= 1
+
+        # difference should be 0, if match all count
+        for key in d.values():
+            if key != 0:
+                return False
+        return True
